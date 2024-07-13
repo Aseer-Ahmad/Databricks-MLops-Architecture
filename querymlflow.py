ed52bb943da22222fea02fd9cc654601ee4e827e
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2024-07-13 20:30:53 +0200

    model query

--- querymlflow.py
+++ querymlflow.py
@@ -1,5 +1,15 @@
-loaded_summarizer = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
+import mlflow
 
-loaded_summarizer.predict(xsum_sample["document"][0])
+def queryMLFlowModel(model_info):
+    """
+    Load a model stored in Python function format
+    URI can be either a Git repository URI or a local path
+    """
+    model = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
+    return model
 
-results = loaded_summarizer.predict(xsum_sample.to_pandas()["document"])
+def predictSample(model, data):
+    return model.predict(data["document"][0])
+
+def predictBatch(model, data):
+    return model.predict(data.to_pandas()["document"])
